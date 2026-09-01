a0fa9eb5955bdb3b54f720764ba9ad273aa1f491
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2026-09-01 16:51:24 +0000

    chore(release): bump version to 0.1.4

--- shared/src/commonMain/kotlin/net/blockhost/trestle/app/BuildInfo.kt
+++ shared/src/commonMain/kotlin/net/blockhost/trestle/app/BuildInfo.kt
@@ -1,6 +1,6 @@
 package net.blockhost.trestle.app
 
 object BuildInfo {
-    const val VERSION = "0.1.3"
+    const val VERSION = "0.1.4"
     const val USER_AGENT = "Trestle/$VERSION (net.blockhost.trestle)"
 }
